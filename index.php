69f8aa49c7b1ff2f44d65acb08a77eff93be6365
Aluno <aluno@edu.senac.br>
2018-11-14 21:45:48 -0200

    o index do projeto

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+     <h1>
+     	<?php
+
+     	 $nome = "Geovane";
+
+          echo $nome; //esse é um comnetário
+          /*
+          esse comentário é de 
+          várias linhas de texto e pode aumentar
+          */
+          #outro tipo de comentário
+
+
+             ?>
+     </h1>
+
+
+</body>
+</html>
